88527d020c9a493c93ed21e4a7556f97fc00a10e
Philipp Albrecht <philipp.albrecht@momox.biz>
2021-08-18 14:25:17 +0200

    Implement utils.hex_to_base64()

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
 extern crate base64;
+extern crate hex;
 
-pub fn hex_to_base64(hex: &str) -> Result<&str, base64::DecodeError> {
+pub fn hex_to_base64(hex_string: &str) -> Result<String, hex::FromHexError> {
+    hex::decode(hex_string).and_then(|result| Ok(base64::encode(result)))
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -11,6 +12,6 @@ mod tests {
     fn test_convert_hex_to_base64() {
         let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-        assert_eq!(hex_to_base64(hex).unwrap(), expected);
+        assert_eq!(hex_to_base64(hex), Ok(String::from(expected)));
     }
 }
